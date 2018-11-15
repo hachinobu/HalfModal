793e6739917ffa244c8cff726e226430c9bba6d7
hachinobu <hachinobu@gmail.com>
2018-11-15 20:55:01 +0900

    Add project

---
+++ HalfModal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HalfModal
+//
+//  Created by Takahiro Nishinobu on 2018/11/15.
+//  Copyright © 2018 hachinobu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
